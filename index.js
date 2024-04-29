806b858fb62c36a47259fa5ff11ca6f460dbcb87
Daniel <danielpvpao@gmail.com>
2024-04-29 12:47:20 -0300

    Prevent game start if response field is empty.

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const number = Math.floor(Math.random() * (10 - 1 + 1) + 1)
-let tryNumber = 3
+const number = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+let tryNumber = 3;
 let Question = document.getElementById("Question");
 let respons = document.getElementById('Respons');
 let response;
 let RestantTryes = document.getElementById('RestantTryes');
-Question.innerText ='Qual é o valor que você deseja chutar de 1 a 10?'
-function Setresponse(){
- response = parseInt(respons.value);
- while (tryNumber > 0){
-    tryNumber--;
-    if(response == number){
-         alert('Parabéns você acertou o número!')
-            location.reload()
-        break;
+Question.innerText = 'Qual é o valor que você deseja chutar de 1 a 10?';
+
+function Setresponse() {
+    response = parseInt(respons.value);
+    if (respons.value === "") {
+        alert("Por favor, insira um número antes de enviar.");
+        return
     }
-    if(response < number){
-        Question.innerText ='Esse número é muito baixo';
-        break;
-    } else if(response > number){
-       Question.innerText ='Esse número é muito alto'
-       break;
+    while (tryNumber > 0) {
+        tryNumber--;
+        if (response == number) {
+            alert('Parabéns você acertou o número!');
+            location.reload();
+            break;
+        }
+        if (response < number) {
+            Question.innerText = 'Esse número é muito baixo';
+            break;
+        } else if (response > number) {
+            Question.innerText = 'Esse número é muito alto';
+            break;
+        }
     }
 }
-}
-function UpdateTryes(){
+function UpdateTryes() {
     RestantTryes.textContent = tryNumber;
-    if(tryNumber == 0 && response !== number){
-        alert(`O número era ${number} , não foi dessa vez tente novamente`) 
-        location.reload()
+    if (tryNumber == 0 && response !== number) {
+        alert(`O número era ${number}, não foi dessa vez, tente novamente`);
+        location.reload();
     }
-    console.log(tryNumber)
-    };
-    
+    console.log(tryNumber);
+}
